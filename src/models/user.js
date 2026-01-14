ad1b4706b997599d2ee36970a948adb7e6099fbf
BYUMVUHORE Aimable <aimablebyumvuhore@gmail.com>
2026-01-14 20:56:35 +0200

    backend complete

--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,28 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isMfaActive: {
+      type: Boolean,
+      default: false,
+    },
+    twoFactorSecret: {
+      type: String,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-  isMfaActive: {
-    type: Boolean,
-    required: false,
-  },
-  twoFactorSecret: {
-    type: String,
-  },
-  timestamps: true,
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 export default User;
